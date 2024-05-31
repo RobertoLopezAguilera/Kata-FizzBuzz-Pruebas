2a34dca693624d00b11d8c7dbd1b20c5d33eaee1
RobertoLopezAguilera <98922244+RobertoLopezAguilera@users.noreply.github.com>
2024-05-31 02:47:20 -0600

    Update FizzBuzz.hs

--- src.save0/FizzBuzz.hs
+++ src.save0/FizzBuzz.hs
@@ -1,3 +1,8 @@
-module FizzBuzz where
-fizzbuzz :: Int -> String
-fizzbuzz n = undefined
+module FizzBuzz (fizzBuzz) where
+
+fizzBuzz :: Int -> String
+fizzBuzz n
+  | n `mod` 15 == 0 = "FizzBuzz"
+  | n `mod` 3  == 0 = "Fizz"
+  | n `mod` 5  == 0 = "Buzz"
+  | otherwise       = show n
