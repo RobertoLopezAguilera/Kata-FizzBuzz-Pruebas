48ae5f936dcaec10e76f4fab9736e5f1cf485826
RobertoLopezAguilera <98922244+RobertoLopezAguilera@users.noreply.github.com>
2024-05-31 02:52:35 -0600

    Delete src.save0 directory

--- src.save0/FizzBuzz.hs
+++
@@ -1,8 +0,0 @@
-module FizzBuzz (fizzBuzz) where
-
-fizzBuzz :: Int -> String
-fizzBuzz n
-  | n `mod` 15 == 0 = "FizzBuzz"
-  | n `mod` 3  == 0 = "Fizz"
-  | n `mod` 5  == 0 = "Buzz"
-  | otherwise       = show n
